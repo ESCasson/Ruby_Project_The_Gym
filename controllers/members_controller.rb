00ec7bc13c36c462cf8443c9f0858b95dc9b2d40
Ellie <cassona08@gmail.com>
2019-11-29 15:14:04 +0000

    finished sprint1 completed member show_all add and edit

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -6,13 +6,28 @@ require_relative( '../models/member.rb' )
 
 also_reload( '../models/*' )
 
+#show_all
 get '/members' do
   @members = Member.all()
   erb( :"members/index" )
 end
 
+#save to database
 post '/members' do
   new_member = Member.new(params)
   new_member.save
   redirect to("/members")
+end
+
+#edit i.e. gets the form for editing
+get '/members/:id/edit' do
+  @member = Member.find_by_id(params[:id])
+  erb( :"members/edit" )
+end
+
+#UPDATE
+post '/members/:id' do
+  result = Member.new(params)
+  result.update()
+  redirect to '/members'
 end
--- models/member.rb
+++ models/member.rb
@@ -1,47 +1,61 @@
 require_relative('../db/sql_runner.rb')
 
 class Member
-attr_reader :id
-attr_accessor :first_name, :last_name, :premier_member
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :premier_member
 
-def initialize(details)
-  @id = details['id'].to_i if details['id']
-  @first_name = details['first_name']
-  @last_name = details['last_name']
-  @premier_member = details['premier_member']
-end
-
-def save()
-  sql = "INSERT INTO members (first_name, last_name, premier_member)
-  VALUES ($1, $2, $3) RETURNING id"
-  values = [@first_name, @last_name, @premier_member]
-  result = SqlRunner.run(sql, values)
-  @id = result[0]['id'].to_i()
-end
-
-def self.delete_all()
-  sql = "DELETE FROM members"
-  SqlRunner.run(sql)
-end
-def self.all()
-  sql = "SELECT * FROM members"
-  result = SqlRunner.run(sql)
-  return result.map{|member| Member.new(member)}.reverse
-end
-
-def self.membership_display(membership_details)
-  if membership_details == 't'
-    return 'Premier Member'
+  def initialize(details)
+    @id = details['id'].to_i if details['id']
+    @first_name = details['first_name']
+    @last_name = details['last_name']
+    @premier_member = details['premier_member']
   end
-  return 'Standard Member'
-end
 
-def update(new_first_name, new_last_name, new_premier_member)
-  sql = "UPDATE members SET (first_name, last_name, premier_member)
-  = ($1, $2, $3) WHERE id = $4"
-  values = [new_first_name, new_last_name, new_premier_member, @id]
-  SqlRunner.run(sql, values)
-end
+  def save()
+    sql = "INSERT INTO members (first_name, last_name, premier_member)
+    VALUES ($1, $2, $3) RETURNING id"
+    values = [@first_name, @last_name, @premier_member]
+    result = SqlRunner.run(sql, values)
+    @id = result[0]['id'].to_i()
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM members"
+    SqlRunner.run(sql)
+  end
+
+  def self.all()
+    sql = "SELECT * FROM members"
+    result = SqlRunner.run(sql)
+    return result.map{|member| Member.new(member)}.reverse
+  end
+
+  def self.hello_world
+    p "hello world!"
+  end
+
+  def self.membership_display(membership_details)
+    if membership_details == 't'
+      return 'Premier Member'
+    end
+    return 'Standard Member'
+  end
+
+  def self.find_by_id(id)
+    sql = "SELECT * FROM members WHERE id = $1"
+    values = [id]
+    member = SqlRunner.run(sql,values)
+    result = Member.new(member[0])
+    return result
+  end
+
+  def update()
+    sql = "UPDATE members SET (first_name, last_name, premier_member)
+    = ($1, $2, $3) WHERE id = $4"
+    values = [@first_name, @last_name, @premier_member, @id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 end
